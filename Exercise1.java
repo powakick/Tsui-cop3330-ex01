fb7b0b0baeb5110877e16e1ecb82e50426f346d8
powakick <90665243+powakick@users.noreply.github.com>
2021-09-13 22:28:03 -0400

    Add files via upload

---
+++ Exercise1.java
@@ -0,0 +1,15 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021    Jonathan Tsui
+ */
+import java.util.Scanner;
+ public class Exercise1{
+    public static void main(String[] args){
+        String usersNames;
+        Scanner scan = new Scanner(System.in);
+    System.out.println("What is your name?");
+        usersNames = scan.nextLine();
+
+    System.out.println("Hello" + myname + "nice to meet you.");
+    }
+ }
